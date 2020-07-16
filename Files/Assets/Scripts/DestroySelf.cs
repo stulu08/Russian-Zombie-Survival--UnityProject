ea5d19a5fca7b0289cf9b5ad69996f4dfbfe4b36
Stulu <58867625+stulu08@users.noreply.github.com>
2020-07-16 15:28:32 +0200

    Create DestroySelf.cs

---
+++ Files/Assets/Scripts/DestroySelf.cs
@@ -0,0 +1,19 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class DestroySelf : MonoBehaviour
+{
+    public float LifeTime = 5f;
+    public bool onStart = true;
+    void Start()
+    {
+        if(onStart){
+            Destroy(gameObject, LifeTime);
+        }
+    }
+    public void Destroy(float LifeTime)
+    {
+        Destroy(gameObject, LifeTime);
+    }
+}
