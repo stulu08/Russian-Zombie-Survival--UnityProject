b173a2e88588a438e72803afcc9728f9bb95ca76
Stulu <58867625+stulu08@users.noreply.github.com>
2020-07-16 15:28:11 +0200

    Create ammocollector.cs

---
+++ Files/Assets/Scripts/ammocollector.cs
@@ -0,0 +1,25 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class ammocollector : MonoBehaviour
+{
+    public string NameOfWeapon;
+    public int Ammo = 30;
+    GameObject Weapon;
+    private void Start()
+    {
+
+    }
+    private void OnCollisionEnter(Collision collision)
+    {
+        if(collision.collider.tag == "Nice Team")
+        {
+            Weapon = GameObject.Find("Player/MainCamera/WeaponHolder/" + NameOfWeapon);
+            if(Weapon.activeSelf){
+                Weapon.GetComponent<Gun>().collectetAmmo += Ammo;
+                GameObject.Destroy(gameObject);
+            }
+        }
+    }
+}
