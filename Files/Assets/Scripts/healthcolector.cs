c573cfe086cef8fad783eaf3df4bb6dcc3d381ac
Stulu <58867625+stulu08@users.noreply.github.com>
2020-07-16 15:28:42 +0200

    Create healthcolector.cs

---
+++ Files/Assets/Scripts/healthcolector.cs
@@ -0,0 +1,18 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class healthcolector : MonoBehaviour
+{
+    public string NameOfObject;
+    GameObject Weapon;
+    private void OnCollisionEnter(Collision collision)
+    {
+        if (collision.collider.tag == "Player")
+        {
+            Weapon = GameObject.Find(NameOfObject);
+            Weapon.GetComponent<HealthPlayer>().currenthealth += 20;
+            GameObject.Destroy(gameObject);
+        }
+    }
+}
