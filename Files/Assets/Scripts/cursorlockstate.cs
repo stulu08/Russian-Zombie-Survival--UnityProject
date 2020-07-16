4866797047200d811965350e83d5a37856968f4c
Stulu <58867625+stulu08@users.noreply.github.com>
2020-07-16 15:28:22 +0200

    Create cursorlockstate.cs

---
+++ Files/Assets/Scripts/cursorlockstate.cs
@@ -0,0 +1,28 @@
+﻿using UnityEngine;
+
+public class cursorlockstate : MonoBehaviour
+{
+    private void Start()
+    {
+        Time.timeScale = 1;
+    }
+    void Update()
+    {
+        if (Input.GetKeyDown(KeyCode.Escape))
+        {
+            PauseGame();
+        }
+
+    }
+
+    public void PauseGame()
+    {
+        Cursor.lockState = CursorLockMode.None;
+        Cursor.visible = true;
+    }
+    public void ResumeGame()
+    {
+        Cursor.lockState = CursorLockMode.Locked;
+        Cursor.visible = false;
+    }
+}
