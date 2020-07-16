596ccc00d01f5899e8d3fe01fe55d3301761da50
Stulu <58867625+stulu08@users.noreply.github.com>
2020-07-16 15:28:27 +0200

    Create DeathMatchStart.cs

---
+++ Files/Assets/Scripts/DeathMatchStart.cs
@@ -0,0 +1,29 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class DeathMatchStart : MonoBehaviour
+{
+    public GameObject[] ObjectsToenable;
+    public GameObject OwnCanvas;
+    void Start()
+    {
+        OwnCanvas.SetActive(true);
+        for(int i = 0; i != ObjectsToenable.Length; i++)
+        {
+            ObjectsToenable[i].SetActive(false);
+        }
+        
+        Time.timeScale = 0;
+    }
+    public void StartGame()
+    {
+        OwnCanvas.SetActive(false);
+        for (int i = 0; i != ObjectsToenable.Length; i++)
+        {
+            ObjectsToenable[i].SetActive(true);
+        }
+
+        Time.timeScale = 1;
+    }
+}
