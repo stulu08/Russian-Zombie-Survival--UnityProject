8c11fe385fada833c3b35d9e0187977156c456e9
Stulu <58867625+stulu08@users.noreply.github.com>
2020-07-16 15:28:17 +0200

    Create buttons.cs

---
+++ Files/Assets/Scripts/buttons.cs
@@ -0,0 +1,33 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class buttons : MonoBehaviour
+{
+    public GameObject[] CanvasToClose;
+    public GameObject[] CanvasToOpen;
+    public void CanvasOpen()
+    {
+        OpenCanvas(CanvasToClose, CanvasToOpen);
+    }
+    public void LoadScene(int SceneIndex)
+    {
+        SceneManager.LoadScene(SceneIndex);
+    }
+    public void Quit()
+    {
+        Application.Quit();
+    }
+    public void Restart()
+    {
+        SceneManager.LoadScene(SceneManager.GetActiveScene().name);
+    }
+    public void OpenCanvas(GameObject[] CanvasToClose, GameObject[] CanvasToOpen)
+    {
+        CanvasToClose[1].gameObject.SetActive(false);
+        CanvasToClose[0].gameObject.SetActive(false);
+
+        CanvasToOpen[0].gameObject.SetActive(true);
+    }
+}
