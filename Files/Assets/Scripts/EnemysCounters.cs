686ebf18e1be05f8f12ff841f97d4f598f49d2e3
Stulu <58867625+stulu08@users.noreply.github.com>
2020-07-16 15:28:36 +0200

    Create EnemysCounters.cs

---
+++ Files/Assets/Scripts/EnemysCounters.cs
@@ -0,0 +1,26 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class EnemysCounters : MonoBehaviour
+{
+    public float EnemysLeft = 0;
+    public bool enable = true;
+    void Update()
+    {
+        if(enabled){
+            GameObject[] enemies = GameObject.FindGameObjectsWithTag("Enemy");
+            EnemysLeft = enemies.Length;
+            if (Input.GetKeyDown(KeyCode.A))
+            {
+                EnemysLeft--;
+            }
+            if (EnemysLeft == 0)
+            {
+
+            }
+            gameObject.GetComponent<Text>().text = EnemysLeft + " Enemies Left";
+        }
+    }
+}
