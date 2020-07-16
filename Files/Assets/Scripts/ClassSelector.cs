a2363caee47c0613c04d19039d115b2ac9fa7457
Stulu <58867625+stulu08@users.noreply.github.com>
2020-07-16 15:28:19 +0200

    Create ClassSelector.cs

---
+++ Files/Assets/Scripts/ClassSelector.cs
@@ -0,0 +1,35 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class ClassSelector : MonoBehaviour
+{
+    public GameObject Assault;
+    public GameObject AssaultSpawn;
+
+    public GameObject Shotgun;
+    public GameObject ShotGunSpawn;
+
+    public GameObject Heavy;
+    public GameObject HeavySpawn;
+
+    public void ButtonClickAssault()
+    {
+        Instantiate(Assault, AssaultSpawn.transform.position, Quaternion.identity);
+        gameObject.GetComponent<DeathMatchStart>().StartGame();
+    }
+
+    public void ButtonClickShotGun()
+    {
+        Instantiate(Shotgun, ShotGunSpawn.transform.position, Quaternion.identity);
+        gameObject.GetComponent<DeathMatchStart>().StartGame();
+    }
+
+    public void ButtonClickHeavy()
+    {
+        Instantiate(Heavy, HeavySpawn.transform.position, Quaternion.identity);
+        gameObject.GetComponent<DeathMatchStart>().StartGame();
+    }
+}
+
