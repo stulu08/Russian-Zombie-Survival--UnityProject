2828284b065bce8621ee7ca09f0a6541231b48ce
Stulu <58867625+stulu08@users.noreply.github.com>
2020-07-16 15:28:53 +0200

    Create WellenManager.cs

---
+++ Files/Assets/Scripts/WellenManager.cs
@@ -0,0 +1,17 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.Analytics;
+using UnityEngine.UI;
+
+public class WellenManager : MonoBehaviour
+{
+    public void SpawnEnemys(int Anzahl, GameObject[] GegnerTypen)
+    {
+        int a = GegnerTypen.Length;
+        for (int i = 0; i < Anzahl; i++)
+            {
+                Instantiate(GegnerTypen[Random.Range(0, a)], gameObject.transform.position, Quaternion.identity);
+            }
+    }
+}
